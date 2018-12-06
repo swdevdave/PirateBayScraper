330c30922d8212cc80410e605d6610c7c1c7b0ab
swdevdave <swdev.dave@gmail.com>
2018-12-06 00:25:34 -0800

    Quiz done, near finish of project

--- PirateBayScraper/Program.cs
+++ PirateBayScraper/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,20 +15,39 @@ namespace PirateBayScraper
         static string seasonNumberResponse;
         static string hiDefResponse;
         static bool hiDef = true;
-        static string vartempurl = "DCs%20Legends.of.Tomorrow";
         static string fullURL;
         static string updatedName;
         static string updatedSeasonNumber;
-        static int episodesToParse;
+        static int episodeNumber = 01;
+        static string magenetUrlfolder;
 
+        static List<string> namesOfFiles = new List<string>();
+        static List<string> magenetUrl = new List<string>();
 
         static void Main(string[] args)
         {
+            checkIfDirExists();
             ConsoleQuiz();
             URLBuilder();
             GetHtmlAsync();
+           
             Console.ReadLine();
                           
+        }
+
+
+        static void checkIfDirExists()
+        {
+            string folderName = @"\PirateBayMagnets";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            magenetUrlfolder = path + folderName;
+    
+            Console.WriteLine(magenetUrlfolder.ToString());
+            if (!Directory.Exists(magenetUrlfolder))
+            {
+                Directory.CreateDirectory(magenetUrlfolder);
+            }
+           
         }
 
         private static void ConsoleQuiz()
@@ -36,8 +56,6 @@ namespace PirateBayScraper
             showNameResponse = Console.ReadLine().Trim();
             string[] splitName = showNameResponse.Split(' ');
         
-            
-
             for (int i = 0; i < splitName.Length; i++)
             {
                 updatedName += splitName[i] + "%20";
@@ -72,16 +90,19 @@ namespace PirateBayScraper
         static string URLBuilder()
         {
 
-            string seasonEpisode = "S" + seasonNumberResponse + "E01";
+            string seasonEpisode = "s" + updatedSeasonNumber + "e0" + episodeNumber;
             string innerUrl = updatedName  + seasonEpisode;
             string baseUrl = "https://thepiratebay.rocks/search/";
             string endUrl = "/1/99/0";
             fullURL = baseUrl + innerUrl + endUrl;
             Console.WriteLine("\n" + fullURL);
 
+           
             return fullURL;
             
         }
+
+
 
         private static async void GetHtmlAsync()
         {
@@ -109,46 +130,34 @@ namespace PirateBayScraper
             var ScraperMibSize = ScraperHtml[0].Descendants("font")            // PARSED MAGNET LINKS
                 .Where(node => node.GetAttributeValue("class", "")
                 .Equals("detDesc")).ToList();
-
-
-            List<string> names = new List<string>();
-
+            
             foreach (var ScraperName in ScraperNames)
             {
-                string nameList = ScraperName.InnerText.Trim('\n', '\t', '\r');
-                //Console.WriteLine(nameList);
-                //Console.WriteLine();
+                namesOfFiles.Add (ScraperName.InnerText);
 
-
-                names.Add(nameList);
             }
-            Console.WriteLine(names[0]);
+            Console.WriteLine(namesOfFiles[0]);
+            
+
             Console.WriteLine();
-
-
-            List<string> magnetList = new List<string>();
-
+            
             foreach (var ScraperMagnet in ScraperMagentsOuter)
             {
                 string magent = ScraperMagnet.OuterHtml;
                 string[] trimed = magent.Split('"');
 
-                magnetList.Add(trimed[1]);
+                magenetUrl.Add(trimed[1]);         
                                
-            
             }
-            Console.WriteLine(magnetList[0]);
+            Console.WriteLine(magenetUrl[0]);
+            string magnet1 = magenetUrl[0];
+            string showName = showNameResponse.Replace(" ", "_");
+            string textFilePath = magenetUrlfolder + @"\" + showName + "_Magnets" + ".txt";
+            StreamWriter sw = new StreamWriter(textFilePath);
+            sw.Write("test");
 
-            foreach (var MibSize in ScraperMibSize)
-            {
-                Console.WriteLine();
-            }
-
-
-
-                                   
-            Console.WriteLine();
-
+            Console.WriteLine();    
+                
         }
     }
 }
